5b6bb819517d1fccf752b00f60efeb099e332d27
Gaurav Anand <gauravanand67584@gmail.com>
2023-11-30 12:04:50 +0530

    working on typing functionality

--- whatsapp-backend/utils/socketDetails.js
+++ whatsapp-backend/utils/socketDetails.js
@@ -23,7 +23,7 @@ export default function (socket, io) {
       // console.log(chalk.red(`this ${socket.id} is disconnected`));
       io.emit("get-online-user", onlineUsers);
 
-      //! we are using io because as the users got disconnected the socket disconnect too so it is impossible to emit something, to to rectify the problem we are using io instead
+      //! we are using [io] because as the users got disconnected the socket disconnects too so it is impossible to emit something, to rectify the problem we are using [io] instead
     });
   });
 
@@ -39,5 +39,17 @@ export default function (socket, io) {
       if (user._id === message.sender._id) return;
       socket.in(user._id).emit("receive message", message);
     });
+  });
+
+  // typing messages
+  socket.on("typing", (conversation) => {
+    console.log(`this ${conversation} is typing`);
+    socket.in(conversation).emit("typing");
+  });
+
+  // stop typing
+  socket.on("stop typing", (conversation) => {
+    console.log(`this ${conversation} stopped typing`);
+    socket.in(conversation).emit("stop typing");
   });
 }
